6a71632e951428791dd15e6187b131a4b525905a
Aashiyana Patel <aashiyanapatel@rocketmail.com>
2021-03-10 22:05:38 +0000

    Automatically backed up by Learn

--- kickstarter_scraper.rb
+++ kickstarter_scraper.rb
@@ -1,5 +1,11 @@
 # require libraries/modules here
+require 'nokogiri'
+require 'open-uri'
 
 def create_project_hash
   # write your code here
+  html= File.read('fixtures/kickstarter.html')
+  kickstarter = Nokogiri::HTML(html)
+  
+  projects = {}
 end
